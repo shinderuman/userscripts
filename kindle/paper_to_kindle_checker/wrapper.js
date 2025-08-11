7291c62552d9850112a52e79951c0068f92d237d
shinderuman <shinderuman@gmail.com>
2025-08-11 15:21:06 +0900

    refactor: Move all scripts to kindle/ directory for better organization
    - Move common.js to kindle/common.js
    - Move all script directories (new_release_checker, paper_to_kindle_checker, sale_checker) to kindle/
    - Update @require paths in all wrapper.js files to reflect new kindle/ directory structure
    - Maintain existing functionality while improving project organization
    - Fix sale_checker/wrapper.js to reference correct main.js file

--- kindle/paper_to_kindle_checker/wrapper.js
+++ kindle/paper_to_kindle_checker/wrapper.js
@@ -5,8 +5,8 @@
 // @description  紙書籍とKindle版の両方が利用可能な商品をチェックして通知する
 // @author       shinderuman
 // @match        https://www.amazon.co.jp/*
-// @require      file:///Users/shinderuman/src/kindle_bot/userscripts/common.js
-// @require      file:///Users/shinderuman/src/kindle_bot/userscripts/paper_to_kindle_checker/main.js
+// @require      file:///Users/shinderuman/src/kindle_bot/userscripts/kindle/common.js
+// @require      file:///Users/shinderuman/src/kindle_bot/userscripts/kindle/paper_to_kindle_checker/main.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_notification
 // @grant        GM_openInTab
